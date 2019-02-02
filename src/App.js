422d66cb6c5bb0e06e7efb06bcc4d8f464a6f527
Alex Kharouk <akharoukpp@gmail.com>
2019-02-02 13:57:36 +0000

    Integrated firebase into application.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
 import React, { Component } from "react";
+import firebase from "firebase";
 import "./App.css";
+
+require("dotenv").config();
+
+let config = {
+  apiKey: process.env.firebaseAPI,
+  authDomain: process.env.authDomain,
+  databaseURL: "https://cameron-cam.firebaseio.com",
+  projectId: "cameron-cam",
+  storageBucket: "cameron-cam.appspot.com",
+  messagingSenderId: process.env.messagingSenderId
+};
+
+firebase.initializeApp(config);
+const db = firebase.database();
+
+function writeToDb(location, long, lat) {
+  db.ref("location/" + location).set({
+    location,
+    long,
+    lat
+  });
+}
+
+db.ref("location/").on(
+  "value",
+  function(snapshot) {
+    console.log(snapshot.val());
+  },
+  function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  }
+);
+
+writeToDb("london", 52, -23);
+writeToDb("paris", 52, -23);
 
 class App extends Component {
   render() {
@@ -7,20 +43,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>Cameron Cam</h1>
-          <script src="https://www.gstatic.com/firebasejs/5.8.2/firebase.js"></script>
-          <script>
-            // Initialize Firebase
-            var config = {
-              apiKey: "AIzaSyB4WkFwqGzI0mzyyIXPPG1jLP5gyi30FN8",
-              authDomain: "cameron-cam.firebaseapp.com",
-              databaseURL: "https://cameron-cam.firebaseio.com",
-              projectId: "cameron-cam",
-              storageBucket: "cameron-cam.appspot.com",
-              messagingSenderId: "579457343374"
-            };
-            firebase.initializeApp(config);
-            console.log(firebase)
-          </script>
         </header>
       </div>
     );

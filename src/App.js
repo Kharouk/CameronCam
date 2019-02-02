efe9280f0486c1854b7250aa38c4990b9bd808b1
Antonio <antoniodaria@msn.com>
2019-02-02 13:19:57 +0000

    install firbase

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>Cameron Cam</h1>
+          <script src="https://www.gstatic.com/firebasejs/5.8.2/firebase.js"></script>
+          <script>
+            // Initialize Firebase
+            var config = {
+              apiKey: "AIzaSyB4WkFwqGzI0mzyyIXPPG1jLP5gyi30FN8",
+              authDomain: "cameron-cam.firebaseapp.com",
+              databaseURL: "https://cameron-cam.firebaseio.com",
+              projectId: "cameron-cam",
+              storageBucket: "cameron-cam.appspot.com",
+              messagingSenderId: "579457343374"
+            };
+            firebase.initializeApp(config);
+            console.log(firebase)
+          </script>
         </header>
       </div>
     );

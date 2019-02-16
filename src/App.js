4ff7e23309a234ceb4c4499220d0a1261e2ea6f7
Alex Kharouk <akharoukpp@gmail.com>
2019-02-16 18:47:27 +0000

    add ismobile fix:

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import firebase from "firebase";
 import Map from "./components/MapComponent";
 import Header from "./components/Header";
+import { isMobile } from "react-device-detect";
 import "./App.css";
 
 let config = {
@@ -17,13 +18,26 @@ firebase.initializeApp(config);
 const db = firebase.database();
 
 class App extends Component {
+  renderContent = () => {
+    if (isMobile) {
+      return (
+        <div>
+          <h5 style={{ textAlign: "center" }}>
+            Please view this page on desktop
+          </h5>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <Header />
+          <Map db={db} />
+        </div>
+      );
+    }
+  };
   render() {
-    return (
-      <div className="App">
-        <Header />
-        <Map db={db} />
-      </div>
-    );
+    return this.renderContent();
   }
 }
 

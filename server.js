fb4c55cd8f7ce5fdad903086ae06bb979a81ce35
Alex Kharouk <akharoukpp@gmail.com>
2019-03-22 19:14:32 +0000

    change port number

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
